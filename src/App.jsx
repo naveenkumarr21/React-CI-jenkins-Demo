f4bb7e4d794537fe275e536e1e60ac0f092ce897
Naveen kumar <naveenkumar.r@codemagen.com>
2026-06-09 20:26:24 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>React Jenkins CI/CD Demo</h1>
+      <p>Deployed from Jenkins 🚀</p>
+    </div>
+  );
+}
+
+export default App;
