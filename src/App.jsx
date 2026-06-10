a4e7cdaf2d8186c0e8cabd9ffc2cea3bd5889f62
Naveenkumar <127716244+naveenkumarr21@users.noreply.github.com>
2026-06-10 10:18:09 +0530

    Update header text in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>React Jenkins CI/CD Demo test</h1>
+      <h1>React Jenkins CI/CD Demo </h1>
       <p>Deployed from Jenkins 🚀 by naveen</p>
     </div>
   );
